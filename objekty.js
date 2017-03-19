e121d62d53e328d01bbd41104c6bdc44a903de45
Beata.Kusza <anja37@o2.pl>
2017-03-19 14:32:57 +0100

    this bind

--- objekty.js
+++ objekty.js
@@ -239,3 +239,16 @@ logNameAll.cell(this.'global');//wywolywanie metody , wyswietli global: Michaeli
 logNameAll.cell(person1,'person1'); //person1: Piter
 logNameAll.cell(person2,'person2');//person2: Micheal
 ...
+logNameAll.apply(this, ['global']);
+logNameAll.apply(person1, ['person1']);
+logNameAll.apply(person2, ['person2']);
+...
+//tworzy funkcje powiazana z objektem person1
+var logNameAll1 = logNameAll.bind(person1);
+console.log(logNameAll1('person1')); //person1: Piter
+
+var logNameAll2 = logNameAll.bind(person2,'person2');
+console.log(logNameAll2()); //person2: Michael
+
+person2.logName = logNameAll1;
+console.log(person2.logName('person2')); //person2: Piter
