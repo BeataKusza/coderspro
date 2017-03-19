ea42d347736a9de4222307d26a87686f1ac8d46c
Beata.Kusza <anja37@o2.pl>
2017-03-19 10:51:09 +0100

    references type

--- objekty.js
+++ objekty.js
@@ -1,5 +1,5 @@
 objekt instancja klassy
-var obj = var object ()'funkcjie w obiektach to metody'
+var obj = var object ()'funkcje w obiektach to metody'
 literał var obj1 = ()
 var obj = obj.prototype...
 garbage colection odsmiecanie pamieci obj = gabl;?
@@ -16,3 +16,24 @@ car1.factormanual = 'fiat';
 car1.model = 'modelT';
 var arr = ['red', 'green', 'blue'];
 console.log(arr[1]);
+//literały wyrazeń regularnych
+var numbers = (/ds/g)
+var numbers2 = new RegExp(\ds\,'g');
+//dostep do kluczy//
+1.var array  = [];
+array.push[123];
+2.or array['push'](132);
+var method = 'push';
+array[method] (3435);
+//Identifikowanie typow referencyjnych//
+function add(a,b) {
+  return a + b;
+}
+console.log(typeof.add);
+metoda instance of:
+var item = [];
+var obj = ();
+
+console.log(item instanceof Array); //true
+console.log(obj instanceof Object); //true
+console.log(add instanceof function); //true
